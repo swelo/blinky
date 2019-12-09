867bde53d8d8c47a7e85a9c5221e4fbbb26a8f00
Pontus Lundin <lundin.codeitez@gmail.com>
2019-12-09 22:13:43 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,5 @@
 int main() {
-    *((unsigned int *)0x400FE608U) = 0x20U;
-    *((unsigned int *)0x40025400U) = 0x0EU;
-    *((unsigned int *)0x4002551CU) = 0x0EU;
-
+ 
     while (1) {
       //TODO add logic to set the GPIO_PORTF_DIR_R data so R/G/B can be turned on/off
 
