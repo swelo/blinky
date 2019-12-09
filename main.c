5bfebbdf9a4d0e0cdf17dc66235559e1846930bc
Pontus Lundin <lundin.codeitez@gmail.com>
2019-12-09 22:15:08 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 int main() {
+ //TODO Add the needed pointers to the needed addresses to init the microcontroller. 
+ //That is the base, DIR, DEN. You can also include the header file and use those macros/defines if you dont want to declare them yourselfs
  
     while (1) {
       //TODO add logic to set the GPIO_PORTF_DIR_R data so R/G/B can be turned on/off
